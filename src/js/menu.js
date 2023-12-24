43d22c39463e733ef7deb8b0992fd602a653328e
KaterynaKocheharova <kot41901@gmail.com>
2023-12-24 12:53:55 +0200

    added navigation js and id to review section

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,10 @@
 const openModalBtn = document.getElementById('openModalBtn');
 const closeModalBtn = document.getElementById('closeModalBtn');
 const modal = document.getElementById('myModal');
+const link1 = document.getElementById('nav-link-1');
+const link2 = document.getElementById('nav-link-2');
+const link3 = document.getElementById('nav-link-3');
+
 
 function openModal() {
     modal.classList.add('is-open');
@@ -14,3 +18,6 @@ function openModal() {
   // Attach click event listeners to the buttons
   openModalBtn.addEventListener('click', openModal);
   closeModalBtn.addEventListener('click', closeModal);
+  link1.addEventListener('click', closeModal);
+  link2.addEventListener('click', closeModal);
+  link3.addEventListener('click', closeModal);
