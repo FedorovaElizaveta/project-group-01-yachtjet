507765b3694208c32db45155f1e5dac80fc9d6de
KaterynaKocheharova <kot41901@gmail.com>
2023-12-24 17:01:33 +0200

    added anchor links and target: _blank

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,5 @@
+// ==================================== ALL VARIABLES =================================================
+
 const openModalBtn = document.getElementById('openModalBtn');
 const closeModalBtn = document.getElementById('closeModalBtn');
 const modal = document.getElementById('myModal');
@@ -5,19 +7,22 @@ const link1 = document.getElementById('nav-link-1');
 const link2 = document.getElementById('nav-link-2');
 const link3 = document.getElementById('nav-link-3');
 
-// Function to open
+// ==================================== OPENING MODAL FUNCTION ========================================
 function openModal() {
     modal.classList.add('is-open');
   }
   
-  // Function to close the modal
+// ================================= CLOSING MODAL FUNCTION ============================================
   function closeModal() {
     modal.classList.remove('is-open');
   }
 
-  // Attach click event listeners to the buttons
+  // ============================== ATTACHING CLICK EVENTS TO BUTTONS =================================
   openModalBtn.addEventListener('click', openModal);
   closeModalBtn.addEventListener('click', closeModal);
+
+  // ATTACHING CLICK EVENTS TO LINKS, SO THAT THE MODAL CLOSES WHEN WE WANT TO NAVIAGTE THROUGHT THE PAGE
+  
   link1.addEventListener('click', closeModal);
   link2.addEventListener('click', closeModal);
   link3.addEventListener('click', closeModal);
